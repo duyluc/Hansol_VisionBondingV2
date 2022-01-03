b3a1ba48f420499ad318c098670f24c750440552
duy luc <duongduyluc3@gmail.com>
2022-01-03 16:56:42 +0700

    coding for AlarmPage

--- LUserControl/AlarmPage.cs
+++ LUserControl/AlarmPage.cs
@@ -12,9 +12,35 @@ namespace Hansol_VisionBondingV2.LUserControl
 {
     public partial class AlarmPage : UserControl
     {
+        public class AlarmInfo
+        {
+            public string Timeslap;
+            public string Sender;
+            public string Message;
+            public string Level;
+            public string Code = "0x00";
+            public string Note = "None";
+        }
+        static private AlarmPage _instance;
+        //static private List<>
+
         public AlarmPage()
         {
             InitializeComponent();
+        }
+
+        public static AlarmPage Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new AlarmPage();
+                return _instance;
+            }
+
+            set
+            {
+                _instance = value;
+            }
         }
     }
 }
